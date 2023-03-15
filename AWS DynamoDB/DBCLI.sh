30b12cf59c69b78667d53328f3702d31e8a753b4
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-15 11:34:56 +0530

    Testing Scan API with --projection-expression

--- AWS DynamoDB/DBCLI.sh
+++ AWS DynamoDB/DBCLI.sh
@@ -8,3 +8,8 @@ aws dynamodb scan --table-name mystore
 
 # Use Max-Items Parameter:
 aws dynamodb scan --table-name mystore --max-items 1
+
+# Use Projection-Expression Parameter:
+aws dynamodb scan --table-name mystore --projection-expression "created"
+aws dynamodb scan --table-name mystore --projection-expression "category"
+aws dynamodb scan --table-name mystore --projection-expression "colour"
