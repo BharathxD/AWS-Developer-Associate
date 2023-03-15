7e8d2d4066c2491c9f0c9070ff86f25f853951d5
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-15 11:33:37 +0530

    Tested DynamoDB Scan API with --max-items params

--- AWS DynamoDB/DBCLI.sh
+++ AWS DynamoDB/DBCLI.sh
@@ -5,3 +5,6 @@ aws dynamodb batch-write-item --request-items file://mystore.json
 
 # Perform scan of ProductOrders table:
 aws dynamodb scan --table-name mystore
+
+# Use Max-Items Parameter:
+aws dynamodb scan --table-name mystore --max-items 1
