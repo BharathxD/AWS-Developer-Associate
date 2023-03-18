96bf8555f60b8d36a56d13a354201fba9f56ae84
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-18 14:29:03 +0530

    Added some CLI Commands to interact with AWS SQS Service

--- AWS DynamoDB/QUERY.sh
+++ AWS DynamoDB/QUERY.sh
@@ -3,3 +3,5 @@
 # Use Key-Conditions Parameter:
 aws dynamodb query  --table-name mystore --key-conditions '{ "clientid":{ "ComparisonOperator":"EQ", "AttributeValueList": [ {"S": "chris@example.com"} ] } }'
 
+# Use Key-Condition-Expression Parameter:
+aws dynamodb query --table-name mystore --key-condition-expression "clientid = :name" --expression-attribute-values '{":name":{"S":"chris@example.com"}}'
